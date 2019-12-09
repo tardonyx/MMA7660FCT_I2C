b3e6c5d52a8e3423a80659449530217c74d6a154
Andrey Bibea <andreybibea@mail.ru>
2019-12-09 15:53:16 +0300

    Add code files

---
+++ MMA7660FCT_I2C.h
@@ -0,0 +1,73 @@
+#include "USCI_I2C.h"
+
+#ifndef MMA7660FCT_I2C_H_
+#define MMA7660FCT_I2C_H_
+
+// Communication
+#define 		MMA7660FC_ADDR		0x4C		// Main address of the device
+#define 		PRESCALER			4			// CLK prescaler on I2C bus
+
+// Registers
+#define			XOUT_REG			0x00		// 6-bit output value X (read-only)
+#define			YOUT_REG			0x01		// 6-bit output value Y (read-only)
+#define			ZOUT_REG			0x02		// 6-bit output value Z (read-only)
+#define			TILT_REG			0x03		// Tilt Status (read-only)
+#define			SRST_REG			0x04		// Sampling Rate Status
+#define			SPCNT_REG			0x05		// Sleep Count
+#define			INTSU_REG			0x06		// Interrupt Setup
+#define			MODE_REG			0x07		// Mode
+#define			SR_REG				0x08		// Auto-Wake/Sleep and Portrait/Landscape samples per seconds and Debounce Filter
+#define			PDET_REG			0x09		// Tap Detection
+#define			PD_REG				0x0A		// Tap Debounce Count
+
+// Masks
+#define			ALERT_MASK			0x40		// If alert bit is set, data reg should be re-read
+#define			TILT_SHAKE_MASK		0x80		// Device detected a shake
+#define			TILT_TAP_MASK		0x20		// Device detected a tap
+#define			TILT_PoLa_MASK		0x1C		// Device orientation detection
+#define			TILT_BaFro_MASK		0x03		// Lying front/back detection
+
+typedef unsigned char uint_8;
+typedef   signed char  int_8;
+
+int_8 axesData[3];
+
+// Write <data> byte to <reg> register
+void MMA7660_writeReg(uint_8 reg, uint_8 data);
+
+// Read and return data from <reg> register
+uint_8 MMA7660_readReg(uint_8 reg);
+
+// Set MODE register with params defined in MODE struct
+void MMA7660_setMODE(uint_8 params);
+
+// Set INTSU register with params defined in INTSU struct
+void MMA7660_setINTSU(uint_8 params);
+
+// Initialization of MMA7660
+void MMA7660_Init(void);
+
+// reads axes data to <buffer> array (its size must be at least 3 bytes)
+void MMA7660_readAxes(int_8 *buffer);
+
+// check, is a operator falls
+uint_8 MMA7660_isFall(void);
+
+// test function with infinite loop
+void MMA7660_test(void);
+
+#endif /* MMA7660FCT_I2C_H_ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
